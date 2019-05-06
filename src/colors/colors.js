4fa400976dcd85d03f287a9eaa3142899e79910e
greatdolphinls <great.dolphin.ls@gmail.com>
2019-05-06 10:46:23 +0300

    Add colors in src folder.

---
+++ src/colors/colors.js
@@ -0,0 +1,7 @@
+export const BACKGROUND_COLOR = "#12232d";
+export const PANEL_COLOR = "#142c38";
+export const GREY = "#7c8389";
+export const RED = "#972c24";
+export const GREEN = "#0dd664";
+export const DARK_BLUE = "#395d73";
+export const WHITE = "#ffffff";
