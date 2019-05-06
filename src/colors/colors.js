eea0653a39844a8965be87dc2fe0217d8ee35c81
greatdolphinls <great.dolphin.ls@gmail.com>
2019-05-06 10:52:27 +0300

    Change name of colors in colors.js

--- src/colors/colors.js
+++ src/colors/colors.js
@@ -1,7 +1,7 @@
 export const BACKGROUND_COLOR = "#12232d";
 export const PANEL_COLOR = "#142c38";
-export const GREY = "#7c8389";
-export const RED = "#972c24";
-export const GREEN = "#0dd664";
-export const DARK_BLUE = "#395d73";
-export const WHITE = "#ffffff";
+export const GREY_COLOR = "#7c8389";
+export const RED_COLOR = "#972c24";
+export const GREEN_COLOR = "#0dd664";
+export const BLUE_COLOR = "#395d73";
+export const WHITE_COLOR = "#ffffff";
