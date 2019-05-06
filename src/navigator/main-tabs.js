ba025c8ebe679e83a5e9d009c38dea51ea42e55a
greatdolphinls <great.dolphin.ls@gmail.com>
2019-05-06 16:08:40 +0300

    Change style in main-tabs

--- src/navigator/main-tabs.js
+++ src/navigator/main-tabs.js
@@ -24,77 +24,73 @@ const navigatorButtons = {
     }
   ]
 };
+
 const startTabs = () => {
-  Promise.all([
-    Icon.getImageSource("md-map", 30),
-    Icon.getImageSource("ios-share-alt", 30),
-    Icon.getImageSource("ios-menu", 30)
-  ]).then(sources => {
-    Navigation.startTabBasedApp({
-      tabs: [
-        {
-          screen: "heatmap.HomeScreen",
-          label: "HOME",
-          title: "HeatMap",
-          icon: require("@assets/img/icon/home.png"),
-          navigatorButtons: navigatorButtons
-        },
-        {
-          screen: "heatmap.StrengthScreen",
-          label: "STRENGTH",
-          title: "HeatMap",
-          icon: require("@assets/img/icon/strength.png"),
-          navigatorButtons: navigatorButtons
-        },
-        {
-          screen: "heatmap.VolumeScreen",
-          label: "VOLUME",
-          title: "HeatMap",
-          icon: require("@assets/img/icon/volume.png"),
-          navigatorButtons: navigatorButtons
-        },
-        {
-          screen: "heatmap.VolatilityScreen",
-          label: "VOLATILITY",
-          title: "HeatMap",
-          icon: require("@assets/img/icon/volatility.png"),
-          navigatorButtons: navigatorButtons
-        },
-        {
-          screen: "heatmap.SentimentScreen",
-          label: "SENTIMENT",
-          title: "HeatMap",
-          icon: require("@assets/img/icon/sentiment.png"),
-          navigatorButtons: navigatorButtons
-        }
-      ],
-      tabsStyle: {
-        tabBarButtonColor: DISABLED_COLOR,
-        tabBarSelectedButtonColor: POSITIVE_COLOR,
-        tabBarBackgroundColor: PANEL_COLOR,
-        forceTitlesDisplay: true,
-        tabFontFamily: "HelveticaLT67MC",
-        tabFontSize: 14,
-        selectedTabFontSize: 14
+  Navigation.startTabBasedApp({
+    tabs: [
+      {
+        screen: "heatmap.HomeScreen",
+        label: "HOME",
+        title: "HeatMap",
+        icon: require("@assets/img/icon/home.png"),
+        navigatorButtons: navigatorButtons
       },
-      appStyle: {
-        tabBarButtonColor: DISABLED_COLOR,
-        tabBarSelectedButtonColor: POSITIVE_COLOR,
-        tabBarBackgroundColor: PANEL_COLOR,
-        forceTitlesDisplay: true,
-        tabFontFamily: "HelveticaLT67MC",
-        tabFontWeight: 400,
-        tabFontSize: 14,
-        selectedTabFontSize: 14,
-        navBarHidden: true,
-        navBarTitleTextCentered: true,
-        navBarBackgroundColor: BACKGROUND_COLOR,
-        navBarTextColor: SUB_FORE_COLOR,
-        navBarButtonColor: SUB_FORE_COLOR,
-        navBarTextFontSize: 25,
-        navBarTextFontFamily: "HelveticaLT67MC"
+      {
+        screen: "heatmap.StrengthScreen",
+        label: "STRENGTH",
+        title: "HeatMap",
+        icon: require("@assets/img/icon/strength.png"),
+        navigatorButtons: navigatorButtons
+      },
+      {
+        screen: "heatmap.VolumeScreen",
+        label: "VOLUME",
+        title: "HeatMap",
+        icon: require("@assets/img/icon/volume.png"),
+        navigatorButtons: navigatorButtons
+      },
+      {
+        screen: "heatmap.VolatilityScreen",
+        label: "VOLATILITY",
+        title: "HeatMap",
+        icon: require("@assets/img/icon/volatility.png"),
+        navigatorButtons: navigatorButtons
+      },
+      {
+        screen: "heatmap.SentimentScreen",
+        label: "SENTIMENT",
+        title: "HeatMap",
+        icon: require("@assets/img/icon/sentiment.png"),
+        navigatorButtons: navigatorButtons
       }
-    });
+    ],
+    tabsStyle: {
+      tabBarButtonColor: DISABLED_COLOR,
+      tabBarSelectedButtonColor: POSITIVE_COLOR,
+      tabBarBackgroundColor: PANEL_COLOR,
+      forceTitlesDisplay: true,
+      tabFontFamily: "HelveticaLT67MC",
+      tabFontSize: 14,
+      selectedTabFontSize: 14
+    },
+    appStyle: {
+      screenBackgroundColor: BACKGROUND_COLOR,
+      tabBarButtonColor: DISABLED_COLOR,
+      tabBarSelectedButtonColor: POSITIVE_COLOR,
+      tabBarBackgroundColor: PANEL_COLOR,
+      forceTitlesDisplay: true,
+      tabFontFamily: "HelveticaLT67MC",
+      tabFontWeight: 400,
+      tabFontSize: 14,
+      selectedTabFontSize: 14,
+      navBarHidden: true,
+      navBarTitleTextCentered: true,
+      navBarBackgroundColor: BACKGROUND_COLOR,
+      navBarTextColor: SUB_FORE_COLOR,
+      navBarButtonColor: SUB_FORE_COLOR,
+      navBarTextFontSize: 25,
+      navBarTextFontFamily: "HelveticaLT67MC"
+    }
   });
 };
 
